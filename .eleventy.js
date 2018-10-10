7bd01c28715994eb9c7037a9d54c7436795c76bc
Chris Herold <cmherold@gmail.com>
2018-10-10 14:12:55 -0700

    update gulp

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
+//const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
+
 module.exports = function(config) {
 
   // Add a date formatter filter to Nunjucks
   config.addFilter("dateDisplay", require("./filters/dates.js") );
   config.addFilter("timestamp", require("./filters/timestamp.js") );
   config.addFilter("squash", require("./filters/squash.js") );
+
+  //add versioning to assets
+  //const cacheBusterOptions = {};
+  //config.addPlugin(cacheBuster(cacheBusterOptions));
 
   return {
     dir: {
